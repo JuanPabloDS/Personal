1d29c16cfcf2c11162419edd0ae088cb4cfd3a68
JuanPabloDS <63618603+JuanPabloDS@users.noreply.github.com>
2023-06-23 16:06:00 -0300

    Questions add

---
+++ .history/core/static/js/script_20230623154351.js
@@ -0,0 +1,98 @@
+class MobileNavbar {
+    constructor(mobileMenu, mobileMenuex, navList, navLinks) {
+      this.body = document.querySelector("body");
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.mobileMenuEx = document.querySelector(mobileMenuex);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+      this.hiddenClass = "hidden";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.body.classList.toggle(this.hiddenClass);
+      this.mobileMenuEx.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+      this.mobileMenuEx.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".mobile-menu-ex",
+    ".nav-list",
+    ".nav-list li",
+  );
+  
+  
+  mobileNavbar.init();
+  
+  
+  function closeMenu(){
+    const menu = document.querySelector(".mobile-menu");
+    const menuEx = document.querySelector(".mobile-menu-ex");
+    const navList = document.querySelector(".nav-list");
+    const body = document.querySelector("body")
+  
+    menu.classList.remove("active")
+    menuEx.classList.remove("active")
+    navList.classList.remove("active")
+    body.classList.remove("hidden")
+    mobileNavbar.animateLinks()
+  }
+
+
+// Menu fixed
+var elemento1 = document.getElementById('nav');
+
+// Obtém as dimensões do elemento 1
+var largura = elemento1.offsetWidth;
+var altura = elemento1.offsetHeight;
+
+// Aplica as dimensões ao elemento 2
+var elemento2 = document.getElementById('fixed');
+elemento2.style.width = largura + 'px';
+elemento2.style.height = altura + 'px';
+
+
+// Questions
+
+function captElement(elemento) {
+
+
+  // Acessar os elementos das duas divs
+  var divs = event.target;
+  var elementos = divs.children;
+
+
+  for (var i = 0; i < elementos.length; i++) {
+    console.log("Div filha:", elementos[i]);
+    elementos[0].style.display = 'block'
+
+  }
+}
