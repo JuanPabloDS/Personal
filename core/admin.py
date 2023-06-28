9070fc44cfcd3c620609c532cfe6d27eaf379189
JuanPabloDS <63618603+JuanPabloDS@users.noreply.github.com>
2023-06-28 17:38:01 -0300

    Dynamic registration

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django import forms
+from .models import (Apresentacao, Sobre, SobreDados, Detalhes,Precos, PrecosPlano1, PrecosPlano1Dados,
+PrecosPlano2Dados, PrecosPlano2, Imagem, ImagensLista, Duvidas, DuvidasRespostas, Contato, Cores)
 
-# Register your models here.
+
+class ApresentacaoAdmin(admin.ModelAdmin):
+    max_num = 1
+
+admin.site.register(Apresentacao, ApresentacaoAdmin)
